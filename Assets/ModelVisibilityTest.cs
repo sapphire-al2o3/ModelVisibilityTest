7ac8b7a201b9d91eee2462b704af9d143a5b17b7
sapphire-al2o3 <t_telkun@hotmail.com>
2020-12-23 20:26:14 +0900

    Initial commit

---
+++ Assets/ModelVisibilityTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelVisibilityTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void SetActive(bool enable)
+	{
+		gameObject.SetActive(enable);
+	}
+
+	void SetRenderer(bool enable)
+	{
+		GetComponent<Renderer>().enabled = enable;
+	}
+
+	void SetLayer(bool enable)
+	{
+		gameObject.layer = LayerMask.NameToLayer(enable ? "Default" : "Hidden");
+	}
+
+	void SetScale(bool enable)
+	{
+		transform.localScale = enable ? Vector3.one : Vector3.zero;
+	}
+}
