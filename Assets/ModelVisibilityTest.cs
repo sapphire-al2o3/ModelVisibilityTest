6419582b48ec9d55ef7a8b7d1ed1812e73a9e0a2
sapphire-al2o3 <t_telkun@hotmail.com>
2020-12-24 10:56:24 +0900

    update button event

--- Assets/ModelVisibilityTest.cs
+++ Assets/ModelVisibilityTest.cs
@@ -1,14 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelVisibilityTest : MonoBehaviour
 {
+	[SerializeField]
+	Button activeButton;
+
+	[SerializeField]
+	Button rendererButton;
+
+	[SerializeField]
+	Button layerButton;
+
+	[SerializeField]
+	Button alphaButton;
+
+	[SerializeField]
+	Button scaleButton;
+
+	bool activeToggle = true;
+	bool rendererToggle = true;
+	bool layerToggle = true;
+	bool scaleToggle = true;
+	bool alphaToggle = true;
+
+	Color defaultColor;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
+		activeButton.onClick.AddListener(() =>
+		{
+			activeToggle = !activeToggle;
+			SetActive(activeToggle);
+		});
+
+		rendererButton.onClick.AddListener(() =>
+		{
+			rendererToggle = !rendererToggle;
+			SetRenderer(rendererToggle);
+		});
+
+		layerButton.onClick.AddListener(() =>
+		{
+			layerToggle = !layerToggle;
+			SetLayer(layerToggle);
+		});
+
+		alphaButton.onClick.AddListener(() =>
+		{
+			alphaToggle = !alphaToggle;
+			SetAlpha(alphaToggle);
+		});
+
+		scaleButton.onClick.AddListener(() =>
+		{
+			scaleToggle = !scaleToggle;
+			SetScale(scaleToggle);
+		});
+
+		defaultColor = GetComponent<Renderer>().material.color;
+	}
 
     // Update is called once per frame
     void Update()
@@ -34,5 +88,10 @@ public class ModelVisibilityTest : MonoBehaviour
 	void SetScale(bool enable)
 	{
 		transform.localScale = enable ? Vector3.one : Vector3.zero;
+	}
+
+	void SetAlpha(bool enable)
+	{
+		GetComponent<Renderer>().material.color = enable ? defaultColor : Color.clear;
 	}
 }
